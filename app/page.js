f37465b46c81a3c007a622e331dbcda5796b863e
GPsync1129 <gpsync.official@gmail.com>
2024-10-01 20:22:34 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -124,7 +124,7 @@ export default function Home() {
                 >
                   <div className={`md:mt-48 ${darkMode ? 'text-white' : 'text-black'}`}>
                     <Typewriter
-                      words={["Capture Moments✨, Secure Memories.📱"]}
+                      words={["Capture Moments, Secure Memories."]}
                       loop={1}
                       typeSpeed={70}
                       deleteSpeed={50}
