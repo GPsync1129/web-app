24f1b7228a68c315bf62c422bb53c149b3a3736c
Om <kmcamja@gmail.com>
2024-10-02 09:16:42 +0530

    favicon added

--- app/layout.js
+++ app/layout.js
@@ -26,7 +26,10 @@ export default function RootLayout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content={metadata.description} />
         <meta name="keywords" content="Google Photos, Download, Metadata" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="app/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="app/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="app/favicon-16x16.png"/>
+        <link rel="manifest" href="app/site.webmanifest"/>
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
