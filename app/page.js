c52050561960ef96d817c1ae43311d1c1dfb5102
Om <kmcamja@gmail.com>
2024-10-04 07:08:53 +0530

    img added

--- app/page.js
+++ app/page.js
@@ -62,9 +62,9 @@ export default function Home() {
         {
           name: "Shreekant Sureliya",
           role: "Developer@ONGC",
-          image: "/cr2.png",
+          image: "/shree.jpg",
           socials: {
-            linkedin: "https://www.linkedin.com/in/om-chaudhari-38960721b/",
+            linkedin: "https://www.linkedin.com/in/shreekant-sureliya-69aaa3125/",
             github: "https://www.linkedin.com/in/om-chaudhari-38960721b/",
           },
         },
