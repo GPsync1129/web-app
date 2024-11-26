9b146bcd58e8e5d3e8fc568aab041a02d4044f92
Om <90174038+omchaudhari1107@users.noreply.github.com>
2024-11-26 08:25:55 +0530

    .deb update

--- app/page.js
+++ app/page.js
@@ -83,7 +83,7 @@ export default function Home() {
     if (os === "windows") {
       fileUrl = "https://www.dropbox.com/scl/fi/jz7rrdzuaxjoejgpyz5ih/gpsync.exe?rlkey=f3dna7jn17z599hg8ndu22poz&st=8ucouqqp&dl=1";
     } else if (os === "linux") {
-      fileUrl = "https://www.dropbox.com/scl/fi/0qktmzv9ve9gb5lp4p50o/gpsync.deb?rlkey=daknm2may04v9f8fu9a9pobzl&st=utx30o5w&dl=1";
+      fileUrl = "https://www.dropbox.com/scl/fi/kvf907hpfy9j5nyczu9lk/gpsync.deb?rlkey=fpinlkb1b5lm207cvufsmssyq&st=qq9u7gjs&dl=1";
     } else {
       console.error("Unsupported OS selected.");
       return;
